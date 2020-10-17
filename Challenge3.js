0c9eb171f739f8d424becd9bebc1c0318fd82679
Preciousoge <61980399+Preciousoge@users.noreply.github.com>
2020-10-17 04:53:44 +0100

    Created reverseNumber fxn
    Created the function to reverse the number.
    
    Co-Authored-By: Amira Adediran <amiradediran@gmail.com>

--- Challenge3.js
+++ Challenge3.js
@@ -15,3 +15,11 @@ Output Example:
 
 Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :) */
 
+function reverseNumber(num)
+{
+
+	num = num + " ";
+	return num.split("").reverse().join("")
+}
+	//reverseNumber(234)
+	console.log(reverseNumber(234))
